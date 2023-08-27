849dbbb959a150a1001dbc6a5b3790545fbf872b
Linggar Juwita <linggarjuwita@gmail.com>
2023-08-27 14:30:04 +0800

    Added binarySearch method

---
+++ internal/data_structures/binary_search.go
@@ -0,0 +1,29 @@
+package datastructures
+
+import (
+	"errors"
+)
+
+
+func binarySearch(array []int, value int) (int, error) {
+	lower := 0
+	upper := len(array) - 1
+
+	for lower <= upper {
+		midPoint := (lower + upper) / 2
+
+		if midValue := array[midPoint]; midValue > value {
+			upper = midPoint - 1
+		} else if midValue < value {
+			lower = midPoint + 1
+		} else {
+			return midPoint, nil
+		}
+
+	}
+
+	return 0, errors.New("not found")
+}
+
+
+
---
+++ internal/data_structures/binary_search_test.go
@@ -0,0 +1,69 @@
+package datastructures
+
+import (
+	"testing"
+)
+
+func Test_binarySearch(t *testing.T) {
+	t.Parallel()
+	
+	type args struct {
+		array []int
+		value int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+
+		{
+			args: args{
+				array: []int{1, 2, 3, 4, 5},
+				value: 6,
+			},
+			want:    0,
+			wantErr: true,
+		},
+		{
+			args: args{
+				array: []int{1, 2, 3, 4},
+				value: 3,
+			},
+			want:    2,
+			wantErr: false,
+		},
+		{
+			args: args{
+				array: []int{1, 2, 3, 4, 5},
+				value: 3,
+			},
+			want:    2,
+			wantErr: false,
+		},
+		{
+			args: args{
+				array: []int{1, 2, 3, 4, 5, 6},
+				value: 3,
+			},
+			want:    2,
+			wantErr: false,
+		},
+	}
+	for _, test := range tests {
+		tt := test
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			
+			got, err := binarySearch(tt.args.array, tt.args.value)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("binarySearch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("binarySearch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
